45c66e6a1f9ff469b15dab20a833e250b3e75890
ShadyShade <theshadyshade@mail.ru>
2025-10-06 01:53:52 +0300

    GitHub Pages Setup

--- scripts/menu/ending.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-@export var win_sfx: AudioStreamPlayer
-var main_menu_scene: String = "res://scenes/menu/main_menu.tscn"
-
-func _on_back_pressed() -> void:
-	SceneTransition.change_scene(main_menu_scene)
